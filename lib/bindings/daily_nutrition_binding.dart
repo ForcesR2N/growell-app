11af1ef26c97ba8243f9f3423b84c9b9b45eb54f
ForcesR2N <muhammadzuhrizal07@gmail.com>
2025-03-10 00:00:29 +0700

    fix controller

--- lib/bindings/daily_nutrition_binding.dart
+++ lib/bindings/daily_nutrition_binding.dart
@@ -4,6 +4,6 @@ import '../controllers/daily_nutrition_controller.dart';
 class DailyNutritionBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put<DailyNutritionController>(DailyNutritionController(), permanent: true);
+    Get.put<DailyNutritionController>(DailyNutritionController());
   }
 }
